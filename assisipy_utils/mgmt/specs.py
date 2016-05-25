d78bbf0cd704cacc57f98e6b1f7cd12d7b55c827
Rob Mills <rob.mills@zepler.net>
2016-05-25 18:42:13 +0100

    implemented yaml reader and writer for agent handler specs
    - interface is equal to before, but the functions should be changed
      (to use the suffix _yaml) -- so aware of change.
    
    - Tested via roundtrip conversions; not used in earnest as yet.

--- assisipy_utils/mgmt/specs.py
+++ assisipy_utils/mgmt/specs.py
@@ -13,8 +13,41 @@ They are recorded in CSV form, with one line per agent. The spec includes:
 
 '''
 
+import yaml
 import csv
+import copy
 
+def read_agent_handler_data_yaml(fname, ty_filter=None, verb=False):
+    '''
+    read specification as written in yaml
+
+    uses specification parser that is maintained with writer!
+    (which provides structured data)
+
+    returns a list of dicts, each dict having the following:
+        name
+        ty
+        pose
+        pa
+        sa
+        exec_script
+        local_conf
+    '''
+    _y = {}
+    with open(fname) as f:
+        _y = yaml.safe_load(f)
+
+    data = []
+    for k, v in _y.iteritems():
+        ty = v.get('type')
+        if ty_filter is not None and ty.lower() != ty_filter:
+            continue
+        else:
+            d = copy.deepcopy(v)
+            d['name'] = k
+            data.append(d)
+
+    return data
 
 def read_agent_handler_data(fname, ty_filter=None, verb=False):
     '''
@@ -51,7 +84,7 @@ def read_agent_handler_data(fname, ty_filter=None, verb=False):
             _d = {
                 'name'        : name,
                 'type'        : ty,
-                'pose'        : (x, y, theta),
+                'pose'        : [float(x), float(y), float(theta)],
                 'pub_addr'    : pub_addr,
                 'sub_addr'    : sub_addr,
                 'exec_script' : exec_script,
@@ -75,6 +108,37 @@ def gen_spec_str(name, obj_type, pose, exec_script, conf,
     s = ", ".join(
         [str(field) for field in [obj_type, name, x, y, yaw, pub_addr, sub_addr, exec_script, conf]
          ])
+    return s
+
+
+def write_spec_file(fname, speclist ):
+    '''
+    writes a list of specification lines to a specfile.
+    '''
+    with open(fname, 'w') as f:
+        for s in speclist:
+            f.write(s+"\n")
+    # done
+
+def gen_spec_str_yaml(name, obj_type, pose, exec_script, conf,
+                  pub_addr='tcp://localhost:5556',
+                  sub_addr='tcp://localhost:5555',
+                  ):
+    '''
+    generate a specification string for an agent. returns a string -- does not
+    write directly to file
+    '''
+    d = {}
+    d[name] = {
+    'type'          : obj_type,
+    'pose'          : list(pose),
+    'exec_script'   : exec_script,
+    'conf'          : conf,
+        'pub_addr'  : pub_addr,
+        'sub_addr'  : sub_addr,
+    }
+
+    s = yaml.safe_dump(d)
     return s
 
 
